0a9d607f8f6a2aa834a291af1d0708dce5b1571d
M1-Masa <masa455.wv@gmail.com>
2022-05-20 07:34:52 +0900

    add:formの訂正項目の訂正、placeholder,select_button,必須

---
+++ build/web/体重.js
@@ -0,0 +1,23 @@
+let weight01 = document.querySelector('.weight01');
+let weight02 = document.querySelector('.weight02');
+
+/**
+ * selectのoptionタグを生成するための関数
+ * @param {Element} elem 変更したいselectの要素
+ * @param {Number} val 表示される文字と値の数値
+ */
+ function createOptionForElements(elem, val) {
+  let option = document.createElement('option');
+  option.text = val;
+  option.value = val;
+  elem.appendChild(option);
+}
+
+//年の生成
+for(let i = 1; i <= 30; i++) {
+  createOptionForElements(weight01, i);
+}
+//月の生成
+for(let i = 0; i <= 9; i++) {
+  createOptionForElements(weight02, i);
+}
